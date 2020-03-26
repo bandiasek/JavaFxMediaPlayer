ff1bac9eadba6761ee4489f3fd7017b41da18b18
bandiasek <51334691+bandiasek@users.noreply.github.com>
2020-03-26 14:52:32 +0100

    Druhý tutorial je hotový
    Druhy tutorial som splnil. Link zde
    https://www.youtube.com/watch?v=78Eq67godTk

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.scene.media.Media;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -19,15 +20,36 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String path = new File ("src/MediaToPlay/videoToPlayOndrej.mp4").getAbsolutePath();
+        String path = new File ("src/MediaToPlay/videoToPlayDamian.mp4").getAbsolutePath();
         media = new Media(new File(path).toURI().toString());
         mediaPlayer = new MediaPlayer(media);
         mediaView.setMediaPlayer(mediaPlayer);
-        mediaPlayer.setAutoPlay(true);
         DoubleProperty width = mediaView.fitWidthProperty();
         DoubleProperty height =mediaView.fitHeightProperty();
         width.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
         height.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
-
     }
+
+    public void play(javafx.event.ActionEvent event){
+        mediaPlayer.play();
+        mediaPlayer.setRate(1);
+    }
+
+    public void pause(javafx.event.ActionEvent event){
+        mediaPlayer.pause();
+    }
+
+    public void slow(javafx.event.ActionEvent event){
+        mediaPlayer.setRate(0.5);
+    }
+
+    public void fast(javafx.event.ActionEvent event){
+        mediaPlayer.setRate(2);
+    }
+
+    public void reload(javafx.event.ActionEvent event){
+        mediaPlayer.seek(mediaPlayer.getStartTime());
+    }
+
+
 }
